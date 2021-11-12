7da7fb0c62517bab2ee3449fff4ae24f0f2e2473
Caio Dalo <caiodalo93@gmail.com>
2021-11-11 21:19:02 -0300

    feat:adicionado requisição para dinamismo das mensagens da home

--- web/src/components/MessageList/index.tsx
+++ web/src/components/MessageList/index.tsx
@@ -1,12 +1,23 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { api } from '../../services/api'
 
 import styles from './styles.module.scss'
 
+type Message = {
+    id: string;
+    text: string;
+    user: {
+        name: string;
+        avatar_url: string;
+    }
+}
+
 export function MessageList() {
+    const [messages, setMessages] = useState<Message[]>([])
+
     useEffect(() => {
-        api.get('messages/last3').then(response => {
-            console.log(response.data)
+        api.get<Message[]>('messages/last3').then(response => {
+            setMessages(response.data)
         })
     }, [])
 
@@ -29,37 +40,20 @@ export function MessageList() {
             </defs>
             </svg>
 
-
             <ul className={styles.messageList}>
-                <li className={styles.message}>
-                    <p className={styles.messageContent}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, officiis.</p>
-                    <div className={styles.messageUser}>
-                        <div className={styles.userImage}>
-                            <img src="https://github.com/CaioDalo.png" alt="Caio Daló" />
-                        </div>
-                        <span>Caio Daló</span>
-                    </div>
-                </li>
-
-                <li className={styles.message}>
-                    <p className={styles.messageContent}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, officiis.</p>
-                    <div className={styles.messageUser}>
-                        <div className={styles.userImage}>
-                            <img src="https://github.com/CaioDalo.png" alt="Caio Daló" />
-                        </div>
-                        <span>Caio Daló</span>
-                    </div>
-                </li>
-
-                <li className={styles.message}>
-                    <p className={styles.messageContent}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, officiis.</p>
-                    <div className={styles.messageUser}>
-                        <div className={styles.userImage}>
-                            <img src="https://github.com/CaioDalo.png" alt="Caio Daló" />
-                        </div>
-                        <span>Caio Daló</span>
-                    </div>
-                </li>
+                {messages.map(message => {
+                    return (
+                        <li key={message.id} className={styles.message}>
+                            <p className={styles.messageContent}>{message.text}</p>
+                            <div className={styles.messageUser}>
+                                <div className={styles.userImage}>
+                                    <img src={message.user.avatar_url} alt={message.user.name} />
+                                </div>
+                                <span>{message.user.name}</span>
+                            </div>
+                        </li>
+                    )
+                })}
             </ul>
         </div>
     )
